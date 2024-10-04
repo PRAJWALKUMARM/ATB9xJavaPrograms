5ff7594dc4eb4ae33fb1ad58eb079c3d50df9faf
PRAJWAL <prajwal.046rm7@gmail.com>
2024-10-04 06:45:17 +0530

    I want to push all my programs.

---
+++ src/Oct/ex_02102024_MainMethod/Lab003.java
@@ -0,0 +1,15 @@
+package Oct.ex_02102024_MainMethod;
+
+public class Lab003 {
+    public static void main(String[] args) {
+        int a=10, b=5;
+        int c= a+b;
+        int d= a-b;
+        int e= a*b;
+        int f= a/b;
+        System.out.println(c);
+        System.out.println(d);
+        System.out.println(e);
+        System.out.println(f);
+    }
+}
