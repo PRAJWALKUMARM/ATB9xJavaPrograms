c501ac5e6ee02c92e4896ca527f491ff0c12acf4
PRAJWAL <prajwal.046rm7@gmail.com>
2024-10-15 20:36:40 +0530

    I want to push all my programs.

---
+++ src/Oct/Lab062.java
@@ -0,0 +1,11 @@
+package Oct;
+
+public class Lab062 {
+    public static void main(String[] args) {
+        int a = 10;
+        System.out.println(--a + a + a--);
+        System.out.println(a);
+    }
+}
+//27
+//8
